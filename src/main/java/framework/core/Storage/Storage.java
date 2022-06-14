ceab64b5c0e1629cc5143497e5ce2e67e30b6db8
rayhanrashid <12345678asdf>
2022-06-13 21:02:44 -0500

    missed storable and storage file

---
+++ src/main/java/framework/core/Storage/Storage.java
@@ -0,0 +1,10 @@
+package framework.core.Storage;
+
+import java.util.Collection;
+
+public interface Storage< O extends Storable<K>,K> {
+    void create(O obj);
+    void update(O obj);
+    O loadByKey(K key);
+    Collection<O> getAll();
+}
